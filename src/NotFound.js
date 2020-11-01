039843d47c5d07df59374a1f6f2f3d4f206e9ed0
hermilanastacio <hermilanastacio@pinion.ph>
2020-11-01 22:05:56 +0800

    - Test

--- src/NotFound.js
+++ src/NotFound.js
@@ -6,11 +6,11 @@ const NotFound = () => {
       <div className="app-notfound">
         <div className="app-notfound-404">
           <div></div>
-          <h1>404</h1>
+          <h1>505</h1>
         </div>
         <h2>Page not found</h2>
         <p>The page you are looking for might have been removed had its name changed or is temporarily unavailable.</p>
-        <a href="#">home page</a>
+        <a href="/">home page</a>
       </div>
     </div>
   );
